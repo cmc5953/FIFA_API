bca0804baffd5b9babd3af8b10f20b92d83e7bf6
cmc5953 <58630027+cmc5953@users.noreply.github.com>
2020-06-13 17:59:43 -0400

    value added to data

--- app.py
+++ app.py
@@ -24,35 +24,35 @@ db = SQLAlchemy(app)
 @app.route("/")
 def skills_attributes():
    with ENG.connect() as con:
-    rs = con.execute('SELECT Name, Age, Height, Weight, Nationality, Overall, Potential, Club, Position FROM soccer_data')
+    rs = con.execute('SELECT Name, Age, Height, Weight, Nationality, Value, Overall, Potential, Club, Position FROM soccer_data')
 
     return jsonify({'Result': [dict(r) for r in rs]})
 
 @app.route("/GK")
 def goalkeeper():
    with ENG.connect() as con:
-    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Overall, Potential, Club FROM soccer_data WHERE Position='GK'")
+    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Value, Overall, Potential, Club FROM soccer_data WHERE Position='GK'")
 
     return jsonify({'Result': [dict(r) for r in rs]})
 
 @app.route("/DEF")
 def defender():
    with ENG.connect() as con:
-     rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Overall, Potential, Club FROM soccer_data WHERE Position='DEF'")
+     rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Value, Overall, Potential, Club FROM soccer_data WHERE Position='DEF'")
 
      return jsonify({'Result': [dict(r) for r in rs]})
 
 @app.route("/MID")
 def midfield():
    with ENG.connect() as con:
-    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Overall, Potential, Club FROM soccer_data WHERE Position='MID'")
+    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Value, Overall, Potential, Club FROM soccer_data WHERE Position='MID'")
 
     return jsonify({'Result': [dict(r) for r in rs]})
 
 @app.route("/FWD")
 def attack():
    with ENG.connect() as con:
-    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Overall, Potential, Club FROM soccer_data WHERE Position='FWD'")
+    rs = con.execute("SELECT Name, Age, Height, Weight, Nationality, Value, Overall, Potential, Club FROM soccer_data WHERE Position='FWD'")
 
     return jsonify({'Result': [dict(r) for r in rs]})
 
