b51dea360c8c410d2635fa8c401c13142407d752
cmc5953 <58630027+cmc5953@users.noreply.github.com>
2020-06-13 15:17:58 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ from flask import (
     jsonify,
     request,
     redirect)
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///soccer_data.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///soccer_data.db"
+ENG = create_engine("sqlite:///./soccer_data.db")
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,16 +21,25 @@ db = SQLAlchemy(app)
 
 @app.route("/api")
 def skills_attributes():
-    results = db.session.query(soccer_data.Name, soccer_data.Wage).all()
+   with ENG.connect() as con:
+    rs = con.execute('SELECT Name, Wage FROM soccer_data')
+    # for r in rs:
+    #     print(r)
 
-    name = [result[0] for result in results]
-    salary = [result[1] for result in results]
+    # name = [rs[0] for r in rs]
+    # name = [result[0] for result in results]
+    # salary = [result[1] for result in results]
 
-    api_display = [{
-        "Player": name,
-        "Salary": salary,
-    }]
-    return jsonify(api_display)
+    # api_display = rs
+    # return jsonify(api_display)
+
+    
+    return jsonify({'Result': [dict(r) for r in rs]})
+
+    # api_display = [{
+    #     "Player": rs,
+    # }]
+    # return jsonify(api_display)
 
 
 if __name__ == "__main__":
